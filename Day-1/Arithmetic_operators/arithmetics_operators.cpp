5a619700498a90b078f73dfcef429919b7c9f252
Mehmet Ali Koylu <koylu_m.ali@outlook.com>
2024-11-10 01:24:57 +0300

    Cpp-progress

---
+++ Day-1/Arithmetic_operators/arithmetics_operators.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+float x= 5; // int x=5;
+int y = 2;
+int a=5;
+float z = a/y;
+cout << z << endl;
+// 5/2 = 2.5 =>2 
+
+// arithmetic operations +,-,*,/,%,pow
+cout<< x+y<<endl;
+cout<< x-y<<endl;
+cout<< x*y<<endl;
+cout<< x/y<<endl;
+cout<< x+true << endl;
+cout<< x+'A' << endl; // ASCII character 'A' 
+cout<< 'B'+'A'<< endl; 
+cout<< pow(x,y) << endl; 
+}
+
---
+++ Day-1/Multiple_veriables/Multiple_veriables.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    //int x;  
+    //int y;
+    //int z;
+
+    int x=35,y=45,z=20;
+    
+    //int x,y,z=30;
+    //x=y=z=45;
+    cout << x << " " << y << " " << z <<endl;
+    //print 
+    //and delete the values and print again
+
+
+    //auto a=45.5 ,b=25, c=30.5f;  different values can't use auto
+    auto a=45.5, b=25.6, c=30.5;
+    cout << a << " " << b << " " << c <<endl;
+    //print
+
+
+}
